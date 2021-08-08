63e44ce34dc17b44b2e2451582c2438f9f068845
Egor Badmaev <badmaeve2511@gmail.com>
2021-08-08 23:09:14 +0300

    feat: add solution to the 5th task (LeaderBoard)

--- rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,25 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSMutableArray<NSNumber *> *playerResults = [NSMutableArray<NSNumber *> new];
+    int playerResult;
+    
+    for (int i = 0; i < [playerArray count]; ++i) {
+        playerResult = 1;
+        for (int j = 0; j < [rankedArray count]; ++j) {
+            if ([playerArray[i] intValue] >= [rankedArray[j] intValue]) {
+                break;
+            } else {
+                if (j + 1 == [rankedArray count] || rankedArray[j+1] != rankedArray[j]) {
+                    playerResult += 1;
+                }
+            }
+        }
+        [playerResults addObject:[NSNumber numberWithInt:playerResult]];
+    }
+    
+    return playerResults;
 }
 
 @end
