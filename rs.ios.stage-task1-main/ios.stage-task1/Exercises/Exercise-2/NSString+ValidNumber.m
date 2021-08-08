2795e7b238a30be9baa301168c6a772b06b69663
Egor Badmaev <badmaeve2511@gmail.com>
2021-08-08 19:58:28 +0300

    fix: fix 2nd tasks' solution

--- rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -5,10 +5,14 @@
 - (BOOL)isValidNumber {
     NSUInteger length = [self length];
     
+    if (length <= 0 || length > 100) {
+            return NO;
+    }
+    
     BOOL result = YES;
     
     for (int i = 0; i < length; i++) {
-        if ([[NSCharacterSet decimalDigitCharacterSet] characterIsMember: [self characterAtIndex: i]]) {
+        if (![[NSCharacterSet decimalDigitCharacterSet] characterIsMember: [self characterAtIndex: i]]) {
             result = NO;
             break;  // нашли символ, не входящий в decimalDigitCharacterSet => NO с break, чтобы последний символ-цифра не перезаписал значение на YES
         }
