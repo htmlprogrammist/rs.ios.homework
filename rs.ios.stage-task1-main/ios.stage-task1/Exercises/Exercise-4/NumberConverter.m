cc6b3918e5fcce2b550aa13acd82f62d752b7220
Egor Badmaev <badmaeve2511@gmail.com>
2021-08-08 20:34:15 +0300

    feat: add solution to the 4th task (NumberConverter)

--- rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,17 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSInteger x = ABS([number intValue]);
+    NSInteger i = 0;
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    
+    while (x > 0) {
+        result[i] = [NSString stringWithFormat: @"%ld", x % 10];
+        i++;
+        x = x / 10;
+    }
+    
+    return result;
 }
 
 @end
