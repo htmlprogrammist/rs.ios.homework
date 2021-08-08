5a2044e739a4d156b7c633c47b418a4082357112
Egor Badmaev <badmaeve2511@gmail.com>
2021-08-08 19:16:55 +0300

    feat: add solution to the 2nd task (NSString + ValidNumber)

--- rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ rs.ios.stage-task1-main/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,18 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    NSUInteger length = [self length];
+    
+    BOOL result = YES;
+    
+    for (int i = 0; i < length; i++) {
+        if ([[NSCharacterSet decimalDigitCharacterSet] characterIsMember: [self characterAtIndex: i]]) {
+            result = NO;
+            break;  // нашли символ, не входящий в decimalDigitCharacterSet => NO с break, чтобы последний символ-цифра не перезаписал значение на YES
+        }
+    }
+    
+    return result;
 }
 
 @end
