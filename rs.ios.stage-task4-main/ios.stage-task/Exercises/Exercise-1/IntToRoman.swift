3d01df37ee414a10ad53a37e78221064fbcddf10
Egor Badmaev <badmaeve2511@gmail.com>
2021-08-11 16:57:00 +0300

    feat: add solution to the first task (IntToRoman)

--- rs.ios.stage-task4-main/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ rs.ios.stage-task4-main/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -1,8 +1,27 @@
 import Foundation
 
 public extension Int {
-    
     var roman: String? {
-        nil
+        if (self < 1 || self > 3999) {  // Constranumberss
+            return nil
+        }
+        
+        let numbersRoman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV"]
+        let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4]
+        
+        var result = ""
+        var number = self
+        
+        for (i, n) in numbersRoman.enumerated() {
+            if number >= numbers[i] {
+                result = result + String(repeating: n, count: number/numbers[i])
+                number = number - (number/numbers[i]) * numbers[i]
+            }
+        }
+        if number < 4 {
+            result = result + String(repeating: "I", count: number)
+        }
+        
+        return result
     }
 }
