81b615d5848dc36a6e0f28b1e941a166fb112c7e
Egor Badmaev <badmaeve2511@gmail.com>
2021-10-23 13:40:13 +0300

    feat: add solution to 3.1 task

--- rs.ios.stage-task5-main/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ rs.ios.stage-task5-main/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -7,7 +7,7 @@ public final class Knapsack {
     let drinks: [Supply]
     let foods: [Supply]
     var maxKilometers: Int {
-        findMaxKilometres()
+        maxKilometres()
     }
     
     init(_ maxWeight: Int, _ foods: [Supply], _ drinks: [Supply]) {
@@ -16,7 +16,34 @@ public final class Knapsack {
         self.foods = foods
     }
     
-    func findMaxKilometres() -> Int {
-        return -1
+    func maxKilometres() -> Int {
+        let foodLength = foods.count
+        let drinkLength = drinks.count
+        
+        let food = knapSack(limit: maxWeight, wt: foods, value: foods, n: foodLength)
+        let drink = knapSack(limit: maxWeight, wt: drinks, value: drinks, n: drinkLength)
+        var maxKilometres: [Int] = []
+        
+        for i in 0...maxWeight {
+            maxKilometres.append(min(food[foodLength][i], drink[drinkLength][maxWeight-i]))
+        }
+        return maxKilometres.max() ?? 0
+    }
+    
+    func knapSack(limit: Int, wt: [Supply], value: [Supply], n: Int) -> [[Int]] {
+        var array = Array(repeating: Array(repeating: 0, count: limit + 1), count: n + 1)
+        
+        for i in 0...n {
+            for j in 0...limit {
+                if i == 0 || j == 0 {
+                    array[i][j] = 0
+                } else if wt[i - 1].weight <= j {
+                    array[i][j] = max(value[i - 1].value + array[i - 1][j - wt[i - 1].weight],  array[i - 1][j]);
+                } else {
+                    array[i][j] = array[i - 1][j]
+                }
+            }
+        }
+        return array
     }
 }
