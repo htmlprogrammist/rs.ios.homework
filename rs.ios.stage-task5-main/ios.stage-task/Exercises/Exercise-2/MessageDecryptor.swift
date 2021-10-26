76d0fe5dc77e6b9f0046f326f512512aa9e51ec7
Egor Badmaev <badmaeve2511@gmail.com>
2021-10-26 09:38:51 +0300

    feat: complete 5.2 MessageDecryptor task

--- rs.ios.stage-task5-main/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ rs.ios.stage-task5-main/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,51 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
-    
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var cin = message
+        var cout: String = ""
+        
+        while cin.count > 0 {
+            var item = String(cin.prefix(while: {$0 != "["}))
+            if let multiplier = Int(item) {
+                let firstRange = cin.range(of: item)
+                cin = cin.replacingCharacters(in: firstRange!, with: "")
+                
+                let subMessage = getSubMessage(message: cin)
+                let decryprted = decryptMessage(subMessage)
+                
+                cout.append(String(repeating: decryprted, count: multiplier))
+                
+                let secondRange = cin.range(of: subMessage)
+                cin = cin.replacingCharacters(in: secondRange!, with: "")
+            } else {
+                item = String(cin.first!)
+                if item != "]" && item != "[" {
+                    cout.append(item)
+                }
+                cin = String(cin.dropFirst())
+            }
+        }
+        return cout
+    }
+    
+    func getSubMessage(message: String) -> String {
+        var subMessage = ""
+        var counter = 0
+        
+        for symbol in message {
+            subMessage.append(symbol)
+            if symbol != "]" {
+                if symbol == "[" {
+                    counter += 1
+                }
+            } else {
+                counter -= 1
+                if counter == 0 {
+                    break
+                }
+            }
+        }
+        return subMessage
     }
 }
