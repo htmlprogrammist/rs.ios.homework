21ebed491b1fe849a928d1da9d9dafe489e71fe7
Egor Badmaev <badmaeve2511@gmail.com>
2021-10-30 13:15:15 +0300

    feat: add solution to 6.1 task 'CoronaClass'

--- rs.ios.stage-task6-main/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ rs.ios.stage-task6-main/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -1,18 +1,45 @@
 import Foundation
 
 class CoronaClass {
- 
-     var seats = [Int]()
+    var seats: [Int] {
+        get {
+            return self.seatsArr.sorted()
+        }
+        set {
+            self.seatsArr = newValue
+        }
+    }
+    var seatsArr = [Int]()
+    var studentsNumber: Int
 
-     init(n: Int) {
- 
-     }
-     
-     func seat() -> Int {
-        -1
-     }
-     
-     func leave(_ p: Int) {
-         
-     }
+    init(n: Int) {
+        self.studentsNumber = n
+    }
+    
+    func seat() -> Int {
+        var desk = 0
+        var maxDistance = 0
+        
+        if self.seats.count < self.studentsNumber && self.seats.count != 0 {
+            if (self.seats[0] == 0 && self.seats.count == 1) || self.seats.last != self.studentsNumber - 1 {
+                desk = self.studentsNumber - 1
+            }
+            else {
+                for i in 1..<self.seats.count {
+                    if (self.seats[i] - self.seats[i - 1]) / 2 > maxDistance {
+                        maxDistance = (self.seats[i] - self.seats[i - 1]) / 2
+                        desk = self.seats[i - 1] + maxDistance
+                    }
+                }
+            }
+        }
+        self.seats.append(desk)
+        return desk
+    }
+
+    func leave(_ position: Int) {
+        if let index = self.seats.firstIndex(of: position) {
+            self.seats.remove(at: index)
+        }
+    }
 }
